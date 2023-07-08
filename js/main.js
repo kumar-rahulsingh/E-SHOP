ddaf29aaabccd637fc397592a166b16e8dfce45c
Rahul Singh <101590599+kumar-rahulsingh@users.noreply.github.com>
2023-07-08 09:32:24 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let daysItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
+
+let countDown = () => {
+  let futureDate = new Date("30 August 2023");
+  let currentDate = new Date();
+  let myDate = futureDate - currentDate;
+
+  let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+  let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+  let min = Math.floor(myDate / 1000 / 60) % 60;
+  let sec = Math.floor(myDate / 1000) % 60;
+
+  daysItem.innerHTML=days;
+  hoursItem.innerHTML=hours;
+  minItem.innerHTML=min;
+  secItem.innerHTML=sec;
+
+}
+countDown()
+setInterval(countDown, 1000)
+ 
+//  nav hide
+let navBar=document.querySelectorAll('.nav-link');
+let navCollapse=document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show");
+    })
+})
